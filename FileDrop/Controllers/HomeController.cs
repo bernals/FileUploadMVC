93b022dea1261a39c21b25e30d8e1ce3e648d405
Bernardo <bernals@cenidet.edu.mx>
2019-03-11 07:35:01 -0600

    Versión inicial

---
+++ FileDrop/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileDrop.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SaveUploadedFile()
+        {
+            bool isSavedSuccessfully = true;
+            string fName = "";
+            try
+            {
+                foreach (string fileName in Request.Files)
+                {
+                    HttpPostedFileBase file = Request.Files[fileName];
+                    //Save file content goes here
+                    fName = Guid.NewGuid().ToString(); //file.FileName;
+                    if (file != null && file.ContentLength > 0)
+                    {
+
+                        var originalDirectory = new System.IO.DirectoryInfo(string.Format("{0}Images\\uploaded", Server.MapPath(@"\")));
+                        string pathString = System.IO.Path.Combine(originalDirectory.ToString(), "imagepath");
+                        var fileName1 = System.IO.Path.GetFileName(file.FileName);
+                        bool isExists = System.IO.Directory.Exists(pathString);
+                        if (!isExists)
+                            System.IO.Directory.CreateDirectory(pathString);
+                        var path = string.Format("{0}\\{1}", pathString, fName);
+                        file.SaveAs(path);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                isSavedSuccessfully = false;
+            }
+
+            if (isSavedSuccessfully  )
+                return Json(new { Message = fName });
+            else
+                return Json(new { Message = "Error in saving file" });
+        }
+
+    }
+}
